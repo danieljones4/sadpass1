5bd0838e82c870ac4040de6d9bfacf00af0e407d
tallytutor <Dan@Daniels-MacBook-Air.local>
2015-07-13 23:34:00 -0400

    Added noTouch class

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function() {
 	jQuery(".foreground").wrapInner('<div class="fginner"></div>');
 	jQuery(".fginner").wrapInner('<div class="boxtxt"></div>');
-	
+	if (!("ontouchstart" in document.documentElement)) {
+		document.documentElement.className += "noTouch";
+	}	
 
 });
 
