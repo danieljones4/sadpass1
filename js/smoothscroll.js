72ccfbe9a3f465ad57d8957601f7dfce1cfb589e
danieljones4 <danielleejones4@gmail.com>
2015-07-14 21:08:28 -0400

    Fixed padding for smothscrolling, fixed some content spelling and added video link to nav

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -38,6 +38,26 @@ $(".deadspace a[href^='#']").on('click', function(e) {
 
 });
 
+$(".youtube a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -60
+     }, 600, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
 $("a.navbar-brand[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
