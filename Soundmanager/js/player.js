d4c2d90053d5d124e735b39a6904426e5fb42bdc
tallytutor <Dan@Daniels-MacBook-Air.local>
2015-07-16 03:25:53 -0400

    Started Jumbotron styling, made functional music player

--- Soundmanager/js/player.js
+++ Soundmanager/js/player.js
@@ -1,23 +1,6 @@
-function progress(percent, $element) {
-    var progressBarWidth = percent * $element.width() / 100;
-    $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "% ");
-}
-
-function formatMilliseconds(milliseconds) {
-
-   return milliseconds;
-}
-
 var player = {
    btnPlay: document.querySelector('.player__play'),
    btnStop: document.querySelector('.player__stop'),
-   btnPrevious: document.querySelector('.player__previous'),
-   btnNext: document.querySelector('.player__next'),
-   btnVolumeDown: document.querySelector('.player__volume-down'),
-   btnVolumeUp: document.querySelector('.player__volume-up'),
-   timeElapsed: document.querySelector('.player__time-elapsed'),
-   timeTotal: document.querySelector('.player__time-total'),
-   volume: document.querySelector('.player__volume-info')
 };
 var audio = null;
 
@@ -28,16 +11,10 @@ soundManager.setup({
       audio = soundManager.createSound({
          id: 'audio',
          url: '../Music/MoveOn.mp3',
-         whileloading: function() {
-            player.timeTotal.textContent = formatMilliseconds(audio.durationEstimate);
-         },
-         whileplaying: function() {
-            player.timeElapsed.textContent = formatMilliseconds(audio.position);
-         },
-         onload: function() {
-            player.timeTotal.textContent = formatMilliseconds(audio.duration);
-         },
+
          onfinish: function() {
+            $('.fa').removeClass('fa-pause');
+            $('.fa').addClass('fa-play');
             var event;
             try {
                // Internet Explorer doesn't like this statement
@@ -60,9 +37,13 @@ player.btnPlay.addEventListener('click', function() {
    if (audio.playState === 0 || audio.paused === true) {
      audio.play();
      this.classList.add('is-playing');
+     $('.fa').removeClass('fa-play');
+     $('.fa').addClass('fa-pause');
    } else {
      audio.pause();
      this.classList.remove('is-playing');
+     $('.fa').removeClass('fa-pause');
+     $('.fa').addClass('fa-play');
    }
 });
 
